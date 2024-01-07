44e12a294caaa49f9886c150a5210bd6fe673aec
Rafael Santos <rafa.05125@gmail.com>
2024-01-07 15:32:13 -0300

    IRepository

---
+++ Series/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Series.Interfaces
+{
+    public interface IRepository<T>
+    {
+
+        List<T> Lista();
+
+        T FindById(int id);
+
+        void Add(T entity);
+
+        void Delete(T id); 
+        
+        void Update(int id, T entity);
+
+        int NextId();
+    }
+}
