ad7beeae66930a7760c58aa0e12f178f809e407f
Rafael Santos <rafa.05125@gmail.com>
2024-01-08 20:03:31 -0300

    SerieRepository

---
+++ Series/Classes/SerieRepository.cs
@@ -0,0 +1,40 @@
+﻿using Series.Interfaces;
+
+namespace Series.Classes
+{
+    public class SerieRepository : IRepository<Serie>
+    {
+
+        private List<Serie> seriesList = new List<Serie>();
+
+        public Serie FindById(int id)
+        {
+            return seriesList[id];
+        }
+
+        public List<Serie> Lista()
+        {
+            return seriesList;
+        }
+
+        public void Add(Serie entity)
+        {
+            seriesList.Add(entity);
+        }
+
+        public void Update(int id, Serie entity)
+        {
+            seriesList[id] = entity;
+        }
+
+        public void Delete(int id)
+        {
+            seriesList[id].Delete();
+        }
+
+        public int NextId()
+        {
+            return seriesList.Count;
+        }
+    }
+}
